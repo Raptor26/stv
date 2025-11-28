799851688346022638561848174004606afe65c6
Mickle Isaev <mrraptor26@gmail.com>
2025-11-28 14:58:59 +0300

    feat(utils): добавлены макросы STV_NO_PADDING_NO_OPTIMIZE*

--- include/stv/utils.hpp
+++ include/stv/utils.hpp
@@ -29,6 +29,35 @@ class NonMovable
     NonMovable &operator=(NonMovable &&) = delete;
 };
 
+/// ----------------------------------------------------------------------------
+/// The macros below are useful for preventing optimization of structures and
+/// classes with template parameters.
+///
+/// <pre>
+/// {@code
+/// STV_NO_PADDING_NO_OPTIMIZE_BEGIN
+/// template <typename T>
+/// struct MyStruct {
+///   char a;
+///   int b;
+///   T t;
+/// };
+/// STV_NO_PADDING_NO_OPTIMIZE_END
+/// }
+/// </pre>
+#if defined(__GNUC__) || defined(__clang__)
+
+    #define STV_NO_PADDING_NO_OPTIMIZE_BEGIN _Pragma("pack(push, 1)")
+    #define STV_NO_PADDING_NO_OPTIMIZE_END   _Pragma("pack(pop)")
+#elifdef(_MSC_VER)
+    #define STV_NO_PADDING_NO_OPTIMIZE_BEGIN __pragma(pack(push, 1))
+    #define STV_NO_PADDING_NO_OPTIMIZE_END   __pragma(pack(pop))
+#else
+    #error "Compiler does not support packing directives"
+    #define STV_NO_PADDING_NO_OPTIMIZE_BEGIN
+    #define STV_NO_PADDING_NO_OPTIMIZE_END
+#endif
+
 } // namespace stv
 
 #endif /* UTILS_HPP */
