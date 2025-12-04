248c300caf0001d3a5b2418f4fc0be38ac30eddb
Mickle Isaev <mrraptor26@gmail.com>
2025-12-04 10:46:09 +0300

    refactor(stv):деструктор классов non_movable и non_copyable помечен как protected и удалено ключевое слово virtual

--- include/stv/utils.hpp
+++ include/stv/utils.hpp
@@ -6,27 +6,31 @@ namespace stv {
 class non_copyable
 {
   public:
-    non_copyable()          = default;
-    virtual ~non_copyable() = default;
+    non_copyable() = default;
 
     non_copyable(const non_copyable &)            = delete;
     non_copyable &operator=(const non_copyable &) = delete;
 
     non_copyable(non_copyable &&)            = default;
     non_copyable &operator=(non_copyable &&) = default;
+
+  protected:
+    ~non_copyable() = default;
 };
 
 class non_movable
 {
   public:
-    non_movable()          = default;
-    virtual ~non_movable() = default;
+    non_movable() = default;
 
     non_movable(const non_movable &)            = default;
     non_movable &operator=(const non_movable &) = default;
 
     non_movable(non_movable &&)            = delete;
     non_movable &operator=(non_movable &&) = delete;
+
+  protected:
+    ~non_movable() = default;
 };
 
 /// ----------------------------------------------------------------------------
