0b46924d21278e38bc95cfaa94e47667b09563c2
Mickle Isaev <mrraptor26@gmail.com>
2026-04-13 12:17:35 +0300

    build: исправлена ошибка слияния

--- property.cmake
+++ property.cmake
@@ -55,11 +55,6 @@ function(stv_add_pedantic_compile_options_and_features TARGET_NAME)
             /WX
             >)
 
-  # Только для Clang
-  target_compile_options(
-    ${TARGET_NAME} PRIVATE $<$<CXX_COMPILER_ID:Clang,AppleClang>:
-                           -Wdocumentation >)
-
   # Установка стандартов языков
   target_compile_features(${TARGET_NAME} PRIVATE cxx_std_23 c_std_17)
 
